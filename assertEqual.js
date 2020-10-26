da0a41f0b51980b2ff11408fb7cb05a99ba033b5
Paulo Santiago <p.santiago796@gmail.com>
2020-10-26 20:12:42 +0000

    ES6 template literals method

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,20 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
+  // adds "" before and after actual and expected if they are strings
+  typeof actual === "string" ? actual = `\"${actual}\"` : null;
+  typeof expected === "string" ? expected = `\"${expected}\"` : null;
   if (actual === expected) {
-    console.log("✅✅✅" + " Assertion Passed: " + actual + " === " + expected);
+    // Old concatenate method with +
+    // console.log("✅✅✅" + " Assertion Passed: " + actual + " === " + expected);
+
+    // ES6 method
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑" + " Assertion Failed: " + actual + " !== " + expected);
+    // Old concatenate method with +
+    // console.log("🛑🛑🛑" + " Assertion Failed: " + actual + " !== " + expected);
+
+    // ES6 method
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
