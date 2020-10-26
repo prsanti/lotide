7d417ca6c30c757c78e466ea0ea2319bba062074
Paulo Santiago <p.santiago796@gmail.com>
2020-10-26 21:40:16 +0000

    eslint fix

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   // adds "" before and after actual and expected if they are strings
-  typeof actual === "string" ? actual = `\"${actual}\"` : null;
-  typeof expected === "string" ? expected = `\"${expected}\"` : null;
+  typeof actual === "string" ? actual = `"${actual}"` : null;
+  typeof expected === "string" ? expected = `"${expected}"` : null;
   if (actual === expected) {
     // Old concatenate method with +
     // console.log("✅✅✅" + " Assertion Passed: " + actual + " === " + expected);
