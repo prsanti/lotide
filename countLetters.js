189ed1a63bfbf18ad4dd5ea7d978a0cc373d3981
Paulo Santiago <p.santiago796@gmail.com>
2020-10-28 21:18:01 +0000

    filled countLetters function

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  typeof actual === "string" ? actual = `"${actual}"` : null;
+  typeof expected === "string" ? expected = `"${expected}"` : null;
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let output = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (output[letter]) {
+        output[letter]++;
+      } else {
+        output[letter] = 1;
+      }
+    }
+  }
+  return output;
+};
+
+// Test Cases
+const word = "lighthouse in the house";
+console.log(countLetters(word));
+assertEqual(countLetters(word)["l"], 1);
+assertEqual(countLetters(word)["i"], 2);
+assertEqual(countLetters(word)["g"], 1);
+assertEqual(countLetters(word)["h"], 4);
+assertEqual(countLetters(word)["t"], 2);
+assertEqual(countLetters(word)["o"], 2);
+assertEqual(countLetters(word)["u"], 2);
+assertEqual(countLetters(word)["s"], 2);
+assertEqual(countLetters(word)["e"], 3);
+assertEqual(countLetters(word)["n"], 1);
+assertEqual(countLetters(word)[" "], undefined);
