bd60f385fde1af5fd96d7f305e52209dbeb36a17
Paulo Santiago <p.santiago796@gmail.com>
2020-10-29 18:09:29 +0000

    added inspect()

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -40,9 +40,9 @@ const eqObjects = function(object1, object2) {
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
@@ -50,5 +50,3 @@ const example = { a: '1', b: 2 };
 const expectedExample = { b: 2, a: '1' };
 
 assertObjectsEqual(example, expectedExample);
-
-
