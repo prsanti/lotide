502ed3388148379e7b0964d9e84c0a306cf3c8b6
Paulo Santiago <p.santiago796@gmail.com>
2020-11-02 20:35:47 +0000

    refactored to use M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,41 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-// Test Cases
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  // Test Case 1: Check the returned array elements
+  const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-// Empty array test
-const empty = tail([]);
-assertEqual(empty[0], undefined);
+  it("returns 2 to ensure we get back two elements", () => {
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("returns 'Lighthouse' to ensure the first element is 'Lighthouse'", () => {
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+
+  it("returns 'Labs' to ensure the second element is 'Labs'", () => {
+    assert.strictEqual(result[1], "Labs");
+  });
+
+  // Test Case: 1 element array
+  const oneElementArray = tail([1]);
+  it("returns empty array when inputting an array with one element", () => {
+    assert.deepEqual(oneElementArray, []);
+  });
+
+  // Test Case: Check the original array
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+
+  tail(words);
+
+  it("original array should still have 3 elements", () => {
+    assert.strictEqual(words.length, 3);
+  });
+
+  // Empty array test
+  const empty = tail([]);
+  it("empty array returns an empty array", () => {
+    assert.deepEqual(empty, []);
+  });
+});
